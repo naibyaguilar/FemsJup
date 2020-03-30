f9d739e64fbbe3494a516ce9078c0b603514af0c
naiby <naiby@DESKTOP-JJ43MOL>
2020-03-30 12:44:20 -0600

    nada en realidad

--- WebServisFemsJup/Model.Context.cs
+++ WebServisFemsJup/Model.Context.cs
@@ -46,6 +46,8 @@ namespace WebServisFemsJup
         public virtual DbSet<usuario> usuarios { get; set; }
         public virtual DbSet<score> scores { get; set; }
         public virtual DbSet<color> colors { get; set; }
+        public virtual DbSet<publiporcate> publiporcates { get; set; }
+        public virtual DbSet<usuariosoli> usuariosolis { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
